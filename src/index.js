1cd0381371114a69a0a50bf31ac15890fb1f0a01
himeno <d_himeno@avelio.co.jp>
2018-07-22 17:00:14 +0900

    sass追加

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './components/App.vue';
+import './style.scss';
 
 new Vue({
-  el    : '#app',
+  el: '#app',
   render: h => h(App),
 });
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,52 @@
-const { VueLoaderPlugin } = require('vue-loader');
+const {
+  VueLoaderPlugin
+} = require('vue-loader');
+const MODE = 'development';
+const enabledSourceMap = (MODE === 'development');
 
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'production',
+  mode: MODE,
 
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.css$/,
         use: [
           'vue-style-loader',
           'css-loader'
         ],
       },
+      // Sassファイルの読み込みとコンパイル
+      {
+        test: /\.scss/, // 対象となるファイルの拡張子
+        use: [
+          // linkタグに出力する機能
+          'style-loader',
+          // CSSをバンドルするための機能
+          {
+            loader: 'css-loader',
+            options: {
+              // オプションでCSS内のurl()メソッドの取り込みを禁止する
+              url: false,
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
 
+              // 0 => no loaders (default);
+              // 1 => postcss-loader;
+              // 2 => postcss-loader, sass-loader
+              importLoaders: 2
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // ソースマップの利用有無
+              sourceMap: enabledSourceMap,
+            }
+          }
+        ],
+      },
       {
         test: /\.vue$/,
         loader: 'vue-loader'
@@ -28,7 +60,9 @@ module.exports = {
             // env を指定することで、ES2018 を ES5 に変換。
             // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
             // webpack の Tree Shaking 機能が使えない
-            ['env', {'modules': false}]
+            ['env', {
+              'modules': false
+            }]
           ]
         }
       },
